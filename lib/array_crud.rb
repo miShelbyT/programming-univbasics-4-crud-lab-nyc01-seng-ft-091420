a4c61fe8f0e5806af333b66439cfa03185451a73
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-23 23:12:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   groceries = ["tofu", "bananas", "peanut butter", "apples"]
-  groceries << "bread"
+  groceries<<"bread"
 end
 
 def add_element_to_start_of_array(array, element)
